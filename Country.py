975077538990fb1c862ec1717da2a734c31cef36
jooshd <58038208+jooshd@users.noreply.github.com>
2019-11-21 09:01:46 -0500

    Add files via upload

---
+++ Country.py
@@ -0,0 +1,18 @@
+class Country:
+    ID_TAG = ''
+    GDP = 0
+    ARMY_ASSETS = 0
+    NAVY_ASSETS = 0
+    AIR_FORCE_ASSETS = 0
+    MANPOWER = 0
+    NUCLEAR_CAPABILITIES = 0
+
+    def __init__(self, a,b,c,d,e,f,g):
+        ID_TAG = a
+        GDP = b
+        ARMY_ASSETS = c
+        NAVY_ASSETS = d
+        AIR_FORCE_ASSETS = e
+        MANPOWER = f
+        NUCLEAR_CAPABILITIES = g
+        
