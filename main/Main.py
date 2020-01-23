9e7714451979efd688db424ce87bd6c71f8da234
Josh <jvdevera20@gmail.com>
2020-01-23 09:05:08 -0500

    Updated Main

--- main/Main.py
+++ main/Main.py
@@ -1,10 +1,52 @@
+import numpy as np
+from openpyxl import load_workbook
+from main import Country
+from main import Reader
+
 class Game:
     end_game = False
     print('Welcome to the the Battle Simulator')
     #Add country reader
+    # countries = np.zeros((10, 10))
+    # str = input("Enter country one\n")
+    # wb = load_workbook('CountryData.xlsx')  # opens the Excel file
+    # source = wb.active
+    # ws = wb.copy_worksheet(source)
+    # c1 = Country
+    # c2 = Country
+    #
+    # for i in range(2, 11):
+    #     country = ws.cell(row=i, column=1)
+    #     if (country.value == str):
+    #         c1.ID_TAG = ws.cell(row=i, column=2).value
+    #         c1.GDP = ws.cell(row=i, column=3).value
+    #         c1.ARMY_ASSETS = ws.cell(row=i, column=4).value
+    #         c1.NAVY_ASSETS = ws.cell(row=i, column=5).value
+    #         c1.AIR_FORCE_ASSETS = ws.cell(row=i, column=6).value
+    #         c1.MANPOWER = ws.cell(row=i, column=7).value
+    #         c1.NUCLEAR_CAPABILITIES = ws.cell(row=i, column=8).value
+    #
+    # str = input("Enter country two\n")
+    # for i in range(2, 11):
+    #     country = ws.cell(row=i, column=1)
+    #     if (country.value == str):
+    #         c2.ID_TAG = ws.cell(row=i, column=2).value
+    #         c2.GDP = ws.cell(row=i, column=3).value
+    #         c2.ARMY_ASSETS = ws.cell(row=i, column=4).value
+    #         c2.NAVY_ASSETS = ws.cell(row=i, column=5).value
+    #         c2.AIR_FORCE_ASSETS = ws.cell(row=i, column=6).value
+    #         c2.MANPOWER = ws.cell(row=i, column=7).value
+    #         c2.NUCLEAR_CAPABILITIES = ws.cell(row=i, column=8).value
     #Add maneuver matrix
-
-    moves = [[11, 21], [12, 22], [13, 23], [14, 24], [15], [16]]            #initialize moves
+    maneuvers = np.array(144, dtype='string')
+    wb1 = load_workbook('MilitaryTactics.xlsx')  # opens the Excel file
+    source1 = wb1.active
+    ws = wb1.copy_worksheet(source1)
+    print(ws.cell(row=1,column=1).value)
+    for i in range(2,12):
+        for j in range(2,12):
+            print('not done')
+    moves = np.zeros((10,10))           #initialize moves
     while(end_game == False):
         military_type = input("Entering the following number for:\n[0] Offense\n[1] Defense\n")
         if(military_type == '0'):
